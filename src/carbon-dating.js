73ac5b91aa746374a21cdd16b4a54a75259a92d4
Lada Kozii <lada.vuso@gmail.com>
2020-09-30 13:20:13 +0300

    feat: implement carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,4 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-
+module.exports = function dateSample(sampleActivity) {
+  sampleActivity = +sampleActivity;
+  if (typeof(sampleActivity) !== Number) {
+    return false;
+  } else {
+    const reaction_rate = Math.LN2 / HALF_LIFE_PERIOD;
+    const sample_age = Math.ceil(Math.log (MODERN_ACTIVITY / sampleActivity ) / reaction_rate);
+    return sample_age;
+  }
+};
