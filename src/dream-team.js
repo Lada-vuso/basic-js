1a181f9982b6c7b04189afc31fb02855a3bdce63
Lada Kozii <lada.vuso@gmail.com>
2020-09-30 19:54:15 +0300

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let nameTeam ="";
+  
+  if (!members || !Array.isArray(members)) { 
+    return false; 
+  }
+  
+  for (let i = 0; i < members.length; i++) {
+   if (typeof(members[i]) === "string") {
+      nameTeam += members[i].trim().substring(0,1).toUpperCase();
+    } else {
+      nameTeam = nameTeam;
+    };
+  };
+  if (nameTeam === "") {
+    return false;
+  } else return nameTeam.split('').sort().join('');
 };
