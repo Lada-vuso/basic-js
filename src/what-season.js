42b3432289be2f65af7db0101771c4e4aa396fec
Lada Kozii <lada.vuso@gmail.com>
2020-10-29 12:16:23 +0200

    feat: add solution what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,19 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(date) {
-  if (typeof data === undefined) {
+module.exports = function getSeason(date){
+  if (!date) {
+    console.log('[egc]');
     return 'Unable to determine the time of year!';
   }
-  // } else if (typeof data !== )
+  if (Object.prototype.toString.call(date) !== "[object Date]" || 
+      isNaN(date)) 
+      throw new Error('Something went wrong'); 
+
   let d = new Date(date);
-  let n = d.getMonth();
-  // console.log(n);
+  let n = d.getMonth()+1;
+  
   if (n >= 3 && n < 6) {
     return "spring";
   } else if (n >= 6 && n < 9) {
     return "summer";
   } else if (n >= 9 && n < 12) {
     return "fall";
-  } else return "winter";
- 
+  } else {
+    return "winter";
+  } 
 };
